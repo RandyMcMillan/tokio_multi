42f1e4ac96098849f2bc4e8e92b120230f0b2b5c
randymcmillan <randymcmillan@protonmail.com>
2025-10-01 17:17:29 -0400

    src/bin/marsaglia.rs:add all zero case

--- src/bin/marsaglia.rs
+++ src/bin/marsaglia.rs
@@ -9,7 +9,7 @@ fn main() {
     //    let rand_val = rng.next();
     //    println!("{}: {}", i + 1, rand_val);
     //}
-    let mut rng = Xorshift128::new(0, 0, 0, 0);
+    let mut rng = Xorshift128::new(0, 0, 0, 0); // zero case invokes the use of nanos() function
 
     println!("Generating 10 pseudo-random 32-bit integers from time nanos:");
     for i in 0..10 {
